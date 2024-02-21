13552ecb932b65172fab4f296312249731722c46
Densell-Morales <90972108+Densell-Morales@users.noreply.github.com>
2024-02-20 21:52:30 -0600

    Add files via upload

---
+++ EJERCICIO 18_03_23/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+//Tablas de multiplicar
+/*
+    int numero2= 4;
+    int numero3= 6;
+
+    cout<< "Tabla de multiplicar del 4"<<endl;
+    cout<< endl;
+    for (int i; i <=10 ; i++) {
+        cout<< "4" << "x" << i << "=" << numero2 * i <<endl;
+        cout<<endl;
+    }
+    cout<< "Tabla de multiplicar del 6"<<endl;
+    cout<< endl;
+    for (int i; i <=10 ; i++) {
+        cout<< "6" << "x" << i << "=" << numero3 * i <<endl;
+        cout<<endl;
+    }*/
+//Factorial
+ /*
+    cout << "Ingrese un numero para calcular el factorial del mismo";
+    int fac;
+    double multi = 1;
+    cin >> fac;
+    cout << "EL resultado del factorial ingresado es:" <<endl;
+    for (int i = 1; i <= fac; i++) {
+        multi = multi * i ;
+    }
+    for (int i = 1; i < fac; i++) {
+         cout << i << "x" ;
+    }
+    cout << fac << "=" << multi <<endl;
+*/
+
+
+//primo o no primio
+    /*int nprim;
+    cout << "ingrese un numero del cual quiera saber si es primo o no lo es";
+    cin >> nprim;
+    if (nprim == 2 ) {
+        cout << "este numero es primo";
+        } else
+    if (nprim%2 == 1 ) {
+        cout << "Es un numero primo";}
+    else cout << "este numero no es primo";
+
+    }*/
+//par o impar
+/*int nprim;
+    cout<< "ingrese un numero del cual quiera saber si es par o no lo es";
+    cin>> nprim;
+    if(nprim%2 == 0){
+        cout << "Es un numero par";}
+        else{
+            cout << "es un numero impar";
+        }}
+*/
+//tabla de multiplicar elegida por el usuario
+/*int numeroingresado;
+    cout << "Ingrese un numero para la tabla de multiplicar"<<endl;
+    cin >> numeroingresado;
+    cout<< endl;
+    for (int i; i <=10 ; i++) {
+        cout<< numeroingresado << "x" << i << "=" << numeroingresado * i <<endl;
+        cout<<endl;
+    }}
+*/
+    int numerofijo=1;
+    cout << "Ingrese un numero"<<endl;
+    cout<< endl;
+    for (int i; i <=9 ; i++) {
+        cout << numerofijo + i ;
+    }}
+
+
